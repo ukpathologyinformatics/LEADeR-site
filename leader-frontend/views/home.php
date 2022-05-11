ad1cd72a861bee366b4225009f802b3012948e59
Sam Armstrong <armstrongsam25@gmail.com>
2022-05-11 14:48:03 -0400

    Added bootstrap template code

--- leader-frontend/views/home.php
+++ leader-frontend/views/home.php
@@ -5,10 +5,14 @@ include_once __DIR__ . '/_header.php';
 ?>
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
                 <h1 class="h4">Dashboard</h1>
-                
             </div>
             <div class="row">
                 &nbsp;
+            </div>
+
+            <div class="row">
+                <div class="col-md-6"></div>
+                <div class="col-md-6"></div>
             </div>
 
     

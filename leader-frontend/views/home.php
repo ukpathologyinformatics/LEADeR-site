d6ab8805af7439b4962156b953f5cd8e3f20b5dc
Sam Armstrong <armstrongsam25@gmail.com>
2022-06-02 13:20:33 -0400

    Added stuff and formatted home page

--- leader-frontend/views/home.php
+++ leader-frontend/views/home.php
@@ -9,18 +9,30 @@ include_once __DIR__ . '/_header.php';
     <section class="home">
 
         <h2 class="title-text">What Would You Like To Do Today?</h2>
-        <div class="row">
-            <div class="col-md-4 center-home-sects">
-                <i class="fa fa-plus-circle"></i><br>
-                <h5>New Entry</h5>
+        <div class="row selection-btns">
+            <div class="col-md-4">
+                <a href="/new-entry">
+                    <div class="center-home-sects">
+                        <i class="fa fa-plus-circle"></i><br>
+                        <h5>New Entry</h5>
+                    </div>
+                </a>
             </div>
-            <div class="col-md-4 center-home-sects">
-                <i class="fas fa-bars"></i><br>
-                <h5>View All</h5>
+            <div class="col-md-4">
+                <a href="/view-all">
+                    <div class="center-home-sects">
+                        <i class="fas fa-bars"></i><br>
+                        <h5>View All</h5>
+                    </div>
+                </a>
             </div>
-            <div class="col-md-4 center-home-sects">
-                <i class="fas fa-search"></i><br>
-                <h5>Search</h5>
+            <div class="col-md-4">
+                <a href="/new-query">
+                    <div class="center-home-sects">
+                        <i class="fas fa-search"></i><br>
+                        <h5>Search</h5>
+                    </div>
+                </a>
             </div>
         </div>
 

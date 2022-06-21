a777db982fc187cf7240b98a8ebf29769cb56d2c
Aaron Mullen <aaron.mullen34@gmail.com>
2022-06-21 14:57:42 -0400

    More database changes

--- leader-frontend/controllers/NewEntryController.php
+++ leader-frontend/controllers/NewEntryController.php
@@ -25,13 +25,12 @@ class NewEntryController {
         #var_dump($entry);
 
         $info = array_slice($entry, 9);
-        var_dump($info);
-        #$entry = array();
+        $json_info = json_encode($info);
 
-        //var_dump($entry["file"]);
-        //console.log($entry);
-        //var_dump($entry);
-        DB::run("INSERT INTO patient (file_status, subj_id, first_recorded, last_updated_date, gender, race, ethnicity, dob, entry) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", [$entry["file"], $entry["subject-id"], $entry["start-date"], $entry["end-date"], $entry["gender"], $entry["race"], $entry["ethnicity"], $entry["dob"], $info]);
+        DB::run("INSERT INTO patient (file_status, subj_id, first_recorded, last_updated_date, gender, race, ethnicity, dob, entry) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", [$entry["file"], $entry["subject-id"], $entry["start-date"], $entry["end-date"], $entry["gender"], $entry["race"], $entry["ethnicity"], $entry["dob"], $json_info]);
+        $id = DB::run("SELECT SCOPE_IDENTITY() AS id");
+        var_dump($id);
+        DB::run("INSERT INTO patient_icd (patient_id, icd_code) VALUES (?, ?)", [$id, $entry["icd-codes"]]);
         $ret = array('success' => $success, 'error_message' => $error_message, 'data' => $entry);
         echo json_encode((object) array_filter($ret, function($value) { return $value !== null; }));
     }

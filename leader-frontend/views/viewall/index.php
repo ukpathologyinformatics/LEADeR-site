b7386468f0c78ed66579cd30cdb77432a1894219
Aaron Mullen <aaron.mullen34@gmail.com>
2022-06-23 12:02:01 -0400

    Added datatables

--- leader-frontend/views/viewall/index.php
+++ leader-frontend/views/viewall/index.php
@@ -3,12 +3,16 @@
 $page = 'view-all';
 include_once __DIR__ . '/../_header.php';
 ?>
+
+    <!--<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
+
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>-->
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
         <h1 class="h4">View All</h1>
     </div>
 
     <section id="view-all-page">
-        <table>
+        <table id="view-all-table">
             <thead>
                 <tr>
                     <th>Patient ID</td>
@@ -19,12 +23,9 @@ include_once __DIR__ . '/../_header.php';
             </thead>
             <tbody>
                 <?php
-//                     require_once __DIR__ . '/../utilities/db.php';
-//                     include_once MODELS_DIR . 'User.php';
 
                     $SELECT = DB::run("SELECT * FROM patient");
                     if($SELECT != false) {
-                        #while($rows = mysqli_fetch_array($SELECT)) {
                         while ($rows = $SELECT->fetch(PDO::FETCH_LAZY)) {
                             echo "
                             <tr>
@@ -32,21 +33,24 @@ include_once __DIR__ . '/../_header.php';
                                 <td>".$rows["file_status"]."</td>
                                 <td>".$rows["dob"]."</td>
                                 <td>".$rows["entry"]."</td>
-                            </tr>
-                            ";
+                            </tr>";
                         }
                     }
-                    else
-                    {
+                    else {
                         echo "
                             <tr>
                             <td colspan='3'>Something went wrong with the query</td>
-                            </tr>
-                        ";
+                            </tr>";
                     }
                 ?>
             </tbody>
         </table>
     </section>
+
+    <script type="text/javascript">
+        $(document).ready( function () {
+            $('#view-all-table').DataTable();
+        } );
+    </script>
 <?php
 include_once __DIR__ . '/../_footer.php';

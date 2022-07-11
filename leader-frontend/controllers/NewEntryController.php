47b667ffb3d26296e8c3b4128075118e241630ac
Aaron Mullen <aaron.mullen34@gmail.com>
2022-07-11 09:47:37 -0400

    Updated models/controllers

--- leader-frontend/controllers/NewEntryController.php
+++ leader-frontend/controllers/NewEntryController.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../utilities/db.php';
 include_once MODELS_DIR . 'User.php';
-
+include_once MODELS_DIR . 'NewEntryModel.php';
 
 
 class NewEntryController {
@@ -12,182 +12,31 @@ class NewEntryController {
 
     public static function addEntry(UserSession $userSession) {
         header('Content-Type: application/json');
-
-        $success = false;
-        $error_message = null;
-        $entry = array();
-        foreach($_POST as $key=>$value) {
-            if ($value != ""){
-
-//                 if (array_key_exists($key, $entry)) {
-//
-//                     $entry[$key] = (array) $entry[$key];
-//                     array_push($entry[$key], $value);
-//                 }
-//                 else {
-                $entry[$key] = $value;
-//                 }
-
-            }
-        }
-        $info = array_slice($entry, 9);
-        $json_info = json_encode($info);
-
-        $submit = DB::run("INSERT INTO patient (file_status, subj_id, first_recorded, last_updated_date, gender, race, ethnicity, dob, entry) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", [$entry["file"], $entry["subject-id"], $entry["start-date"], $entry["end-date"], $entry["gender"], $entry["race"], $entry["ethnicity"], $entry["dob"], $json_info]);
-        if ($submit != false) {
-            $success = true;
-        }
-        $stmt = DB::run("SELECT @@IDENTITY AS id");
-        if ($id = $stmt->fetch(PDO::FETCH_LAZY))
-
-        $int_id = (int)$id["id"];
-        DB::run("INSERT INTO patient_icd (patient_id, icd_code) VALUES (?, ?)", [$int_id, $entry["icd-codes"]]);
-
-        if (array_key_exists('left-classification', $entry)) {
-            $codes = explode(",", $entry["left-classification"]);
-            foreach ($codes as $class_name) {
-                DB::run("INSERT INTO patient_class (patient_id, code_id) VALUES (?, ?)", [$int_id, $class_name]);
-            }
-        }
-        if (array_key_exists('right-classification', $entry)) {
-            $codes = explode(",", $entry["right-classification"]);
-            foreach ($codes as $class_name) {
-                DB::run("INSERT INTO patient_class (patient_id, code_id) VALUES (?, ?)", [$int_id, $class_name]);
-            }
-        }
-        if (array_key_exists('lower-left-classification', $entry)) {
-            $codes = explode(",", $entry["lower-left-classification"]);
-            foreach ($codes as $class_name) {
-                DB::run("INSERT INTO patient_class (patient_id, code_id) VALUES (?, ?)", [$int_id, $class_name]);
-            }
-        }
-        if (array_key_exists('lower-right-classification', $entry)) {
-            $codes = explode(",", $entry["lower-right-classification"]);
-            foreach ($codes as $class_name) {
-                DB::run("INSERT INTO patient_class (patient_id, code_id) VALUES (?, ?)", [$int_id, $class_name]);
-            }
-        }
-        if (array_key_exists('lower-right-surgeries', $entry)) {
-            $surgeries = explode("},", $entry["lower-right-surgeries"]);
-
-            $bracket = "}";
-            for($i = 0;$i < count($surgeries)-1;$i++) {
-                $surgeries[$i] = $surgeries[$i] . $bracket;
-            }
-            //var_dump($surgeries);
-            foreach ($surgeries as $operation) {
-                $surgery = json_decode($operation, true);
-                //var_dump($surgery);
-                //var_dump($surgery["cpt"]);
-                DB::run("INSERT INTO patient_surgery (patient_id, surgery_id, surgery_date, surgery_notes, attending_surgeon, age) VALUES (?, ?, ?, ?, ?, ?)", [$int_id, $surgery["cpt"], $surgery["surg-date"], $surgery["notes"], $surgery["surgeon"], $surgery["age"]]);
-
-            }
-            //foreach ($codes as $class_name) {
-            //    DB::run("INSERT INTO patient_class (patient_id, code_id) VALUES (?, ?)", [$int_id, $class_name]);
-            //}
-        }
-
-        if (array_key_exists('lower-left-surgeries', $entry)) {
-            $surgeries = explode("},", $entry["lower-left-surgeries"]);
-
-            $bracket = "}";
-            for($i = 0;$i < count($surgeries)-1;$i++) {
-                $surgeries[$i] = $surgeries[$i] . $bracket;
-            }
-            //var_dump($surgeries);
-            foreach ($surgeries as $operation) {
-                $surgery = json_decode($operation, true);
-                //var_dump($surgery);
-                //var_dump($surgery["cpt"]);
-                DB::run("INSERT INTO patient_surgery (patient_id, surgery_id, surgery_date, surgery_notes, attending_surgeon, age) VALUES (?, ?, ?, ?, ?, ?)", [$int_id, $surgery["cpt"], $surgery["surg-date"], $surgery["notes"], $surgery["surgeon"], $surgery["age"]]);
-
-            }
-            //foreach ($codes as $class_name) {
-            //    DB::run("INSERT INTO patient_class (patient_id, code_id) VALUES (?, ?)", [$int_id, $class_name]);
-            //}
-        }
-
-        $ret = array('success' => $success, 'error_message' => $error_message, 'data' => $entry);
+        $ret = NewEntryClass::addEntry($userSession);
         echo json_encode((object) array_filter($ret, function($value) { return $value !== null; }));
     }
 
     public static function addSurgery(UserSession $userSession) {
         header('Content-Type: application/json');
-        $success = false;
-        $error_message = null;
-        $surg_name = $_POST['surg_name'];
-        $surg_date = $_POST['surg_date'];
-        $surgeon = $_POST['surgeon'];
-        $age = $_POST['age'];
-        $notes = $_POST['notes'];
-        $cpt = $_POST['cpt'];
-        //$submit = DB::run("INSERT INTO classifications (code_id, class_name, location) VALUES (?, ?, ?)", [$code, $name, $location]);
-        //if ($submit != false) {
-        //    $success = true;
-        //}
-
-
-        $ret = array('success' => $success, 'error_message' => $error_message);
+        $ret = NewEntryClass::addSurgery($userSession);
         echo json_encode((object) array_filter($ret, function($value) { return $value !== null; }));
     }
 
     public static function addClassification(UserSession $userSession) {
         header('Content-Type: application/json');
-        $success = false;
-        $error_message = null;
-        $name = $_POST['name'];
-        $code = $_POST['code'];
-        $location = $_POST['location'];
-        $submit = DB::run("INSERT INTO classifications (code_id, class_name, location) VALUES (?, ?, ?)", [$code, $name, $location]);
-        if ($submit != false) {
-            $success = true;
-        }
-
-        $ret = array('success' => $success, 'error_message' => $error_message);
+        $ret = NewEntryClass::addClassification($userSession);
         echo json_encode((object) array_filter($ret, function($value) { return $value !== null; }));
     }
 
     public static function fillClassDropdown(UserSession $userSession) {
         header('Content-Type: application/json');
-        $success = false;
-        $error_message = null;
-        $all_data = array();
-        try {
-            $SELECT = DB::run("SELECT * FROM classifications");
-            if($SELECT != false) {
-                while ($rows = $SELECT->fetch(PDO::FETCH_LAZY)) {
-                    array_push($all_data, array('location' => $rows['location'], 'code_id' => $rows['code_id'], 'class_name' => $rows['class_name']));
-                }
-                $success = true;
-            }
-        } catch (Exception $e) {
-            $message=$e;
-        }
-
-
-        $ret = array('success' => $success, 'error_message' => $error_message, 'data' => $all_data);
+        $ret = NewEntryClass::fillClassDropdown($userSession);
         echo json_encode((object) array_filter($ret, function($value) { return $value !== null; }));
     }
 
     public static function fillSurgeryDropdown(UserSession $userSession) {
         header('Content-Type: application/json');
-        $success = false;
-        $error_message = null;
-        $all_data = array();
-        try {
-            $SELECT = DB::run("SELECT * FROM surgery");
-            if($SELECT != false) {
-                while ($rows = $SELECT->fetch(PDO::FETCH_LAZY)) {
-                    array_push($all_data, array('name' => $rows['surgery_name'], 'code' => $rows['CPT_code']));
-                }
-                $success = true;
-            }
-        } catch (Exception $e) {
-            $message=$e;
-        }
-
-
-        $ret = array('success' => $success, 'error_message' => $error_message, 'data' => $all_data);
+        $ret = NewEntryClass::fillSurgeryDropdown($userSession);
         echo json_encode((object) array_filter($ret, function($value) { return $value !== null; }));
     }
 
--- leader-frontend/controllers/ViewAllController.php
+++ leader-frontend/controllers/ViewAllController.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../utilities/db.php';
 include_once MODELS_DIR . 'User.php';
+include_once MODELS_DIR . 'ViewAllModel.php';
 
 class ViewAllController {
     public static function index(UserSession $userSession) {
@@ -9,43 +10,7 @@ class ViewAllController {
     }
     public static function fillTable(UserSession $userSession) {
         header('Content-Type: application/json');
-        $success = false;
-        $error_message = null;
-        $all_data = array();
-        $patient_ids = array();
-        try {
-            $SELECT = DB::run("SELECT * FROM patient LEFT JOIN patient_icd ON patient.patient_id=patient_icd.patient_id");
-            if($SELECT != false) {
-                while ($rows = $SELECT->fetch(PDO::FETCH_LAZY)) {
-                    if (!in_array($rows['patient_id'], $patient_ids)) {
-                        $classes = DB::run("SELECT STRING_AGG(code_id,', ') AS classes FROM patient_class WHERE patient_id = ?", [$rows['patient_id']]);
-                        if ($class_temp = $classes->fetch(PDO::FETCH_LAZY))
-                        $class_set = $class_temp['classes'];
-
-                        $surgeries = DB::run("SELECT STRING_AGG(surgery_id,', ') AS surgeries FROM patient_surgery WHERE patient_id = ?", [$rows['patient_id']]);
-                        if ($surgery_temp = $surgeries->fetch(PDO::FETCH_LAZY))
-                        $surgery_set = $surgery_temp['surgeries'];
-
-                        if (is_null($class_set)) {
-                            $class_set = "None";
-                        }
-                        if (is_null($surgery_set)) {
-                            $surgery_set = "None";
-                        }
-
-                        array_push($all_data, array('patient_id' => $rows['patient_id'], 'file_status' => $rows['file_status'], 'icd_code' => $rows['icd_code'], 'code_id' => $class_set, 'surgery_id' => $surgery_set));
-                        array_push($patient_ids, $rows['patient_id']);
-                    }
-
-                }
-                $success = true;
-            }
-        } catch (Exception $e) {
-            $message=$e;
-        }
-
-
-        $ret = array('success' => $success, 'error_message' => $error_message, 'data' => $all_data);
+        $ret = ViewAllClass::fillTable($userSession);
         echo json_encode((object) array_filter($ret, function($value) { return $value !== null; }));
     }
 }
